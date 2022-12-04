5a349804e198562103002e10fe2a78bb6a0d46fb
Nnyeka <Nnyeka212@gmail.com>
2022-12-04 14:15:07 -0800

    task-6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for col in range(len(row)):
+    for row in range(len(matrix)):
+        for col in range(len(matrix[row])):
             if col != 0:
                 print(" ", end='')
             print("{:d}".format[matrix[row][col], end='')
