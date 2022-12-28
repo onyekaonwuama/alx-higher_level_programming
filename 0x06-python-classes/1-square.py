531ebf75ba22cd9e673a1c6bb18693c12e8405e4
Nnyeka <nnyeka212@gmail.com>
2022-12-28 22:22:32 +0000

    task-1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,8 +3,8 @@
 """Defines a class Square"""
 
 class Square:
-"""takes the instance of a square"""
+	"""takes the instance of a square"""
 
-def __init__(self, size):
-"""inits a new instance of a square"""
-self.__size = size
+	def __init__(self, size):
+	"""inits a new instance of a square"""
+		self.__size = size
