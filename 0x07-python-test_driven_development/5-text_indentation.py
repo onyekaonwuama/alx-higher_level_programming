75b7b76a85201b97c56cb3b9f241b21a712f54e2
Nnyeka <nnyeka212@gmail.com>
2023-01-02 04:01:41 +0000

    tak6

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defines a function for text indentation."""
+
+
+def text_indentation(text):
+    """Prints text with two new lines after each '.', '?', and ':'.
+    Args:
+        text (str): The text to print.
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
+
+        raise TypeError("text must be a string")
+
+    i = 0
+    while i < len(text) and text[i] == ' ':
+        i += 1
+
+    while i < len(text):
+        print(text[i], end="")
+        if text[i] == "\n" or text[i] in ".?:":
+            if text[i] in ".?:":
+                print("\n")
+            i += 1
+            while i < len(text) and text[i] == ' ':
+                i += 1
+            continue
+        i += 1
