2fedc7300049aa1635cc5f789145f57a450d32bb
Nnyeka <nnyeka212@gmail.com>
2023-01-03 08:46:03 +0000

    task2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -39,7 +39,7 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
              raise ValueError("height must be >= 0")
-             self.__height = value
+        self.__height = value
         
     def area(self):
         """returns the rectangle area"""
