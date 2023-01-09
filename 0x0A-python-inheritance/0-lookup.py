09453ab9e57be317a239c9839122178edfa7bbee
Nnyeka <nnyeka212@gmail.com>
2023-01-09 19:28:49 +0000

    task0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""Defines a function that returns the list of available attributes and methods of an object"""
+
+def lookup(obj):
+"""Return a list of an object's available attributes."""
+    return (dir(obj))
