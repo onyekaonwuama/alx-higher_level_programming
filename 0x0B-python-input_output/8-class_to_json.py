1099b65cdd93486409b5a578e63fd35624ccecf8
Nnyeka <nnyeka212@gmail.com>
2023-01-10 18:02:33 +0000

    task_8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""Defines a Python class-to-JSON function."""
+
+
+def class_to_json(obj):
+    """Returns the dictionary documentation of a simple data structure."""
+    return obj.__dict__
