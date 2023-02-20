4600bf1e31168d0d996933d38209bbf7107bfa12
Nnyeka <nnyeka212@gmail.com>
2023-02-19 21:59:08 -0800

    task_9

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- a script that lists all cities contained in the database hbtn_0d_usa.
+
+SELECT id, name FROM cities
+WHERE state_id IN (
+	SELECT id FROM states
+	WHERE name = 'California')
+ORDER BY id;
